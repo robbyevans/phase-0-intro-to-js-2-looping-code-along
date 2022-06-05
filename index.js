7f4974606711fbf2049ba3f5ec358aa87bce34cc
Evans Rutto <robbyevans001@gmail.com>
2022-06-05 12:34:52 +0300

    commit tree 5762db39e0d94b68ee17519e14c6527a077201b3

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+//arrays noma sana
+
+// const myArray=["jonny", "walker", "jack","Daniel"];
+
+function writeCards(myArray,msg) {
+    let message=[];
+  for (let i = 0; i < myArray.length; i++) {
+    message.push(`Thank you, ${myArray[i]}, for the wonderful ${msg} gift!`);
+}
+return(message);
+
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+// writeCards(["jonny", "walker", "jack","Daniel"],'wassup')
+// writeCards(myArray,'Congratulations!');
+function countDown() {
+    let startNumber = 10;
+    while (startNumber >= 0) {
+      console.log(startNumber--);
+    }
+  }
